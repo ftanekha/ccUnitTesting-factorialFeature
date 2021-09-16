f009c94f34c9e5a8e83115d8023d8a246f3a29f6
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-16 16:26:48 +0100

    getting into the red p2

--- test/index_test.js
+++ test/index_test.js
@@ -4,7 +4,14 @@ Calculate =  require('../index.js')
 describe('Calculate', () => {
     describe('.factorial', () => {
         it('tests if the output of 5! is equal to 120', () => {
-            assert.equal(Calculate.factorial(5),120)
+            const expectedResult = 120,
+            result = Calculate.factorial(5)
+            assert.equal(result, expectedResult)
+        })
+        it('tests if the output of 3! is equal to 6', () => {
+            const expectedResult = 6,
+            result = Calculate.factorial(3)
+            assert.equal(result, expectedResult)
         })
     });
 });
